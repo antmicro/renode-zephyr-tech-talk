5089f6cb36802a41903f4dc8735885c982bddce6
Mateusz Hołenko <mholenko@antmicro.com>
2023-10-11 10:09:06 +0200

    Update 02-pyrenode.py
    Reformat

--- 02-automation/02-pyrenode.py
+++ 02-automation/02-pyrenode.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
 
-
 from pyrenode import connect_renode, get_keywords, shutdown_renode
-connect_renode()
-get_keywords()
 
 uart = 'sysbus.usart1'
+
+connect_renode()
+get_keywords()
 
 ExecuteCommand('i @scripts/single-node/sam_e70.resc')
 CreateTerminalTester(uart)
 ExecuteCommand(f"showAnalyzer {uart}")
+
 StartEmulation()
 WaitForPromptOnUart("uart:~")
 WriteLineToUart("version")
 WaitForLineOnUart("Zephyr* version 1.14.0-rc1", timeout=60, treatAsRegex=True)
 
 print(ExecuteCommand('echo "Test passed!"'))
+
 shutdown_renode()
